874d60f381cab3526f073a734885f13cbcc6ead0
jelmer-agricola <jelmer.agricola@gmail.com>
2023-04-14 10:05:18 +0200

    test

---
+++ src/test/java/com/jelmer/cbackendhomeworkweek9springboottechiteasycontroller/BackendHomeworkWeek9SpringBootTechItEasyControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jelmer.cbackendhomeworkweek9springboottechiteasycontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHomeworkWeek9SpringBootTechItEasyControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
