125b6a04e38320533bdad6e1f11ee052f2404064
jelmer-agricola <jelmer.agricola@gmail.com>
2023-04-14 10:09:59 +0200

    add branch

--- src/main/java/com/jelmer/cbackendhomeworkweek9springboottechiteasycontroller/BackendHomeworkWeek9SpringBootTechItEasyControllerApplication.java
+++ src/main/java/com/jelmer/cbackendhomeworkweek9springboottechiteasycontroller/BackendHomeworkWeek9SpringBootTechItEasyControllerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendHomeworkWeek9SpringBootTechItEasyControllerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendHomeworkWeek9SpringBootTechItEasyControllerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendHomeworkWeek9SpringBootTechItEasyControllerApplication.class, args);
+    }
 
 }
--- src/test/java/com/jelmer/cbackendhomeworkweek9springboottechiteasycontroller/BackendHomeworkWeek9SpringBootTechItEasyControllerApplicationTests.java
+++ src/test/java/com/jelmer/cbackendhomeworkweek9springboottechiteasycontroller/BackendHomeworkWeek9SpringBootTechItEasyControllerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BackendHomeworkWeek9SpringBootTechItEasyControllerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
